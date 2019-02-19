754a747ffc5e7589530014bc29a83e44afe7ce94
Krzysztof Gajdemski <songo@debian.org.pl>
2019-02-19 17:23:43 +0100

    instance-common: add missing domain to instance name

--- instance-common/main.tf
+++ instance-common/main.tf
@@ -12,7 +12,7 @@ resource "openstack_blockstorage_volume_v2" "instance-vol1" {
 # Create an instance
 resource "openstack_compute_instance_v2" "instance" {
   count		  = "${var.count}"
-  name            = "${var.instance-name}-${count.index}"
+  name            = "${var.instance-name}-${count.index}${var.instance-domain}"
   flavor_name     = "${var.flavor-name}"
   key_pair        = "${var.keypair}"
   security_groups = "${var.security-groups}"
